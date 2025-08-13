8a1c299f1b2b270963535f335d13ee2db6d65535
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-13 10:04:04 +0530

    multer initialize

--- server/src/routes/songRouter.js
+++ server/src/routes/songRouter.js
@@ -1,9 +1,12 @@
 import { addSongs,listSongs } from "../controllers/songsController.js";
 import express from 'express';
+import upload from "../middlewares/multer.js";
 
 const songRouter = express.Router();
 
-songRouter.post('/add', addSongs);
+songRouter.post('/add', upload.fields([{name: 'image', maxCount:1}, {name: 'audio', maxCount:1}]) ,addSongs);
+// .fields() is used when you need different types of files uploaded together (e.g., image + audio), each with its own field name.
+
 songRouter.get('/list', listSongs);
 
 export default songRouter;
