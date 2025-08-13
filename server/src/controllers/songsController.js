ea4159027923df404bd5ada364e7f2a359cc9569
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-13 21:44:04 +0530

    pushing and pulling files from database

--- server/src/controllers/songsController.js
+++ server/src/controllers/songsController.js
@@ -15,15 +15,50 @@ const addSongs = async (req,res)=> {
 
         const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image" });
         const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video" });
+        const duration = `${Math.floor(audioUpload.duration/60)} : ${Math.floor(audioUpload.duration%60)}` // minutes : seconds
 
         // console.log(name, desc, album, audioUpload, imageUpload);
+
+        const songData = {
+            name,
+            desc,
+            album,
+            image: imageUpload.secure_url,
+            file: audioUpload.secure_url,
+            duration,
+        }
+        const song = SongsModel(songData)
+        await song.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Song added"
+        })
         
     } catch (error) {
-        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: "Song not added",
+            error: error.message
+        })
     }
 }
 
-const listSongs = async ()=> {
-
+const listSongs = async (req,res)=> {
+    try {
+        const allSongs = await SongsModel.find({});
+        res.status(201).json({
+            success: true,
+            message: "All Songs listed..",
+            songs: allSongs
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to retrieve songs",
+            error: error.message
+        })
+    }
 }
+
 export { addSongs, listSongs }
