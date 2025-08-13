7ce77f9bb5bab4340e18828ce0f58f8e10ceaa09
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-13 18:38:02 +0530

    environment

--- server/src/controllers/songsController.js
+++ server/src/controllers/songsController.js
@@ -1,5 +1,20 @@
-const addSongs = async ()=> {
+import { v2 as cloudinary } from 'cloudinary'
+import { SongsModel } from '../models/songModel.js';
 
+const addSongs = async ()=> {
+    try {
+        const { name, desc, album,}  = req.body;
+        const imageFile = req.files.image[0]; // from upload multer
+        const audioFile = req.files.audio[0]; // from upload multer
+
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video" });
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image" });
+
+        console.log(name, desc, album, audioUpload, imageUpload);
+        
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 const listSongs = async ()=> {
