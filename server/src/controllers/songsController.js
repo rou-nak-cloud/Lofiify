2c0e8afe549e6e455bf7a6f3fb64fe04e41d3837
rou-nak-cloud <github.rounak.19@gmail.com>
2025-08-13 19:37:40 +0530

    trial uploading through multer

--- server/src/controllers/songsController.js
+++ server/src/controllers/songsController.js
@@ -1,16 +1,22 @@
 import { v2 as cloudinary } from 'cloudinary'
 import { SongsModel } from '../models/songModel.js';
 
-const addSongs = async ()=> {
+const addSongs = async (req,res)=> {
     try {
-        const { name, desc, album,}  = req.body;
+         console.log("FILES:", req.files); // Check what Multer parsed
+        console.log("BODY:", req.body);   // Check text fields
+
+        // const { name, desc, album,}  = req.body;
+        const name = req.body.name;
+        const desc = req.body.desc;
+        const album = req.body.album;
         const imageFile = req.files.image[0]; // from upload multer
         const audioFile = req.files.audio[0]; // from upload multer
 
-        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video" });
         const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image" });
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video" });
 
-        console.log(name, desc, album, audioUpload, imageUpload);
+        // console.log(name, desc, album, audioUpload, imageUpload);
         
     } catch (error) {
         console.log(error.message)
--- server/src/middlewares/multer.js
+++ server/src/middlewares/multer.js
@@ -1,10 +1,10 @@
 import multer from 'multer'
 
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null, 'uploads/')
-    },
-    filename: function(req,res,cb){
+    // destination: function(req,file,cb){
+    //     cb(null, 'uploads/')
+    // },
+    filename: function(req,file,cb){
         cb(null, file.originalname)
     },
 })
